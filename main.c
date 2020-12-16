966ec5df26691453258a1590649b483e4ecee93b
Hamza Boucherit <hamzaboucherit19@gmail.com>
2020-12-16 03:20:58 +0100

    initial commit

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(int *argc, char *argv[])
 
     Analex("code_test.txt");
     PROGRAM();
-    // sasasa
+    // sasasasss
     if (Cour_Token.token == FIN_TOKEN)
     {
         printf("BRAVO");
